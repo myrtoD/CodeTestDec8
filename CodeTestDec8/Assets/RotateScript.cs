287f0f2b13b5f69075427d0c0808c27bd649970c
Myrto <diakv115@newschool.edu>
2015-12-13 03:43:13 +0200

    some things

---
+++ CodeTestDec8/Assets/RotateScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+
+	public Transform sphere;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		if (transform.position.x >= 0) {
+			Transform.RotateAround (sphere.position, Vector3.forward, 100 * Time.deltaTime);
+		} else {
+			Transform.RotateAround (sphere.position, -Vector3.forward, 100 * Time.deltaTime);
+		}
+	}
+}
